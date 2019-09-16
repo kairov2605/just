286a5c070e4f7beb6ca91a6b7f363bf02c25bc07
kairov2605 <kairov2605@gmail.com>
2019-09-16 19:27:17 +0300

    Add project files.

---
+++ just/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace just
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HJ");
+            Console.ReadLine();
+        }
+    }
+}
